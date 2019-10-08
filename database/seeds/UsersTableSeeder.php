543d86aad2f20710858053517f9c33742ecc474f
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-09 00:16:13 +0500

    Fix user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,12 +13,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class)->create([
-            'name' => 'Admin',
+        $admin_role = Role::where('name', 'admin')->first();
+        $simple_role = Role::where('name', 'simple_user')->first();
+
+        $admin = User::create([
+            'name' => 'Администратор',
             'email' => 'admin@example.com',
-            'password' => bcrypt('secret'),
-            'isAdmin' => 1,
+            'password' => bcrypt('secret')
         ]);
-        factory(App\User::class, 50)->create();
+        $admin->roles()->attach($admin_role);
+        $simple = User::create([
+            'name' => 'Иван Иванов',
+            'email' => 'vanya@example.com',
+            'password' => bcrypt('12345')
+        ]);
+        $simple->roles()->attach($simple_role);
     }
 }
