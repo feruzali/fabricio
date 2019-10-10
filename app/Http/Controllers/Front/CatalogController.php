6b3eb0282503152af4e7bf66b0dbfc78417582ac
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-10 16:24:16 +0500

    Main action for all catalog, parse url by slug

--- app/Http/Controllers/Front/CatalogController.php
+++ app/Http/Controllers/Front/CatalogController.php
@@ -14,15 +14,39 @@ class CatalogController extends Controller
      * A main action for all catalog actions
      *
      * @param Request $request
+     * @param string $params
      *
      * @return \Illuminate\Http\Response
     */
-    public function index(Request $request)
+    public function index(Request $request, string $params)
     {
-        $categories = Categories::all();
-        $products = Product::paginate(20);
-        $brands = Brands::all();
+        $paramsArray = explode('/', trim($params, '/'));
+        $slug = end($paramsArray);
 
-        return view('front.catalog.index', compact('categories', 'brands', 'products'));
+        $category = Categories::where('slug', '=', $slug)->first();
+        if ($category)
+            return $this->processCategory($request, $category);
+        $product = Product::where('slug', '=', $slug)->first();
+        if ($product)
+            return $this->processProduct($product);
+        abort(404);
+    }
+
+    private function processCategory(Request $request, Categories $category)
+    {
+        $productsQuery = $category->products();
+        $data = [];
+        if ($request->has('brand_id')) {
+            $data['brand_id'] = $request->get('brand_id');
+            $productsQuery = $productsQuery->where('brand_id', '=', $request->get('brand_id'));
+        }
+        $products = $productsQuery->get();
+        $data['products'] = $products;
+        return view('front.catalog.index', $data);
+    }
+
+    private function processProduct(Product $product)
+    {
+        return view('front.catalog.product', compact('product'));
     }
 }
