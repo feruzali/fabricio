b290605702027c05418710a9d49783bc3b892399
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-12 12:08:02 +0500

    Change login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,21 @@
-@extends('layouts.app')
+@extends('front.layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<section class="form" id="form">
+	<legend class="form__title">Typeform</legend>
+	<form action="#" class="form-login">
+		<label class="form-login__label" for="login-email">Email</label>
+		<input class="form-login__input" type="email" name="login-email" placeholder="Companyname@gmail.com" required="">
+		<label class="form-login__label" for="login-password">Password</label>
+		<input class="form-login__input" type="password" name="login-password" placeholder="At least 6 characters" required="">
+		<a href="#" class="form-login_forgot">I forgot my password</a>
+		<button class="form-login__btn">Log in to Typeform</button>
+		<small>This site is protected by reCAPTCHA and the <br> Google Privacy Policy and Terms of Service apply.</small>
+		<span class="form-login_or">Or</span>
+		<div class="form-login_google">
+			<a href="" class="uk-icon-button" uk-icon="icon: google-plus; ratio: 1.6"></a>
+			<span>Sign in with Google</span>
+		</div>
+	</form>
+</section>
 @endsection
