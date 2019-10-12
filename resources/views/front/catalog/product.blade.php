b2b80d257658d51699186fbe69426d66a8f177ec
Feruz <feruzprivet@gmail.com>
2019-10-12 20:23:23 +0500

    Edit card page

--- resources/views/front/catalog/product.blade.php
+++ resources/views/front/catalog/product.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div uk-slideshow>=
                 <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
-                    <ul class="uk-slideshow-items darkk__bg">
+                    <ul class="uk-slideshow-items card-slideshow__bg">
                         @foreach($product->img as $productImage)
                             <li>
                                 <img class="uk-position-center uk-margin-large-left" src="{{ $productImage->getImage() }}" alt="{{ $product->title }}">
@@ -15,45 +15,46 @@
                                     <div class="circle" style="background-color: #f593a7;"><i class="fa fa-check"></i></div>
                                 </div>
                                 <div class="uk-overlay uk-position-bottom-right uk-position-small">
-                                    <p class="darkk">{{ $product->title }}</p>
+                                    <p class="card-slideshow">{{ $product->title }}</p>
                                 </div>
                             </li>
                         @endforeach
                     </ul>
-                    <div class="">
+                    <div class="uk-position-bottom-right uk-position-medium card-slideshow__nav">
                         <i class="fa fa-long-arrow-left" uk-slideshow-item="previous"></i>
                         <i class="fa fa-long-arrow-right" uk-slideshow-item="next"></i>
                     </div>
-                    <button class="darkk__btn"><i class="fa fa-shopping-cart"></i>&nbsp; Add to card</button>
+                    <button class="card-slideshow__btn"><i class="fa fa-shopping-cart"></i>&nbsp; Add to card</button>
                     <div class="uk-position-center-right uk-position-large">
                         <ul class="uk-slideshow-nav uk-dotnav uk-dotnav-vertical"></ul>
                     </div>
                 </div>
             </div>
+            <h2 class="card__title">Fabricio fb1810</h2>
         </div>
     </section>
 
 
-    <section class="descr" id="descr">
+    <section class="card-descr" id="card-descr">
 
         <div class="container">
             <h2 class="section-title">{{ number_format($product->price, $thousands_sep = ' ') }} сум</h2>
 
             <div class="row">
                 <div class="col-4">
-                    <h4>Описание</h4>
-                    <p>{{ $product->description }}</p>
+                    <h4 class="card-descr__title">Описание</h4>
+                    <p class="card-descr__txt">{{ $product->description }}</p>
                 </div>
                 <div class="col-4">
-                    <h4>Доставка</h4>
-                    <p>Free shipping to Uzbekistan
+                    <h4 class="card-descr__title">Доставка</h4>
+                    <p class="card-descr__txt">Free shipping to Uzbekistan
                         30 days returns policy
                         Duty & tax paid (DDP)
                         Five-year warranty</p>
                 </div>
                 <div class="col-4">
-                    <h4>Характеристики</h4>
-                    <p>Designed with care in Sweden and crafted in Pforzheim, Germany using thoughtfully sourced, high-grade components. Movement from the world-renowned Swiss manufacturer Ronda and a 316L steel case produced with a careful combination of brushed and polished finishes. </p>
+                    <h4 class="card-descr__title">Характеристики</h4>
+                    <p class="card-descr__txt">Designed with care in Sweden and crafted in Pforzheim, Germany using thoughtfully sourced, high-grade components. Movement from the world-renowned Swiss manufacturer Ronda and a 316L steel case produced with a careful combination of brushed and polished finishes. </p>
                 </div>
             </div>
 
