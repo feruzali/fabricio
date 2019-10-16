6501839ec998a6d833365ab6b2f5ebc7ad5800df
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-16 20:44:40 +0500

    Add csrf

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
 <section class="form" id="form">
 	<legend class="form__title">Typeform</legend>
 	<form action="#" class="form-login" method="post">
+		@csrf
 		<label class="form-login__label" for="login-email">Email</label>
 		<input class="form-login__input" type="email" name="email" placeholder="Companyname@gmail.com" required="">
 		<label class="form-login__label" for="login-password">Password</label>
