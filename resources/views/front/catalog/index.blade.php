e7f25e5bd1cc5732adb28497f7076d186fc64585
Feruz <feruzprivet@gmail.com>
2019-10-20 21:36:27 +0500

    Add filter dropdowns

--- resources/views/front/catalog/index.blade.php
+++ resources/views/front/catalog/index.blade.php
@@ -5,35 +5,85 @@
             <h2 class="page-title">{{ $category->ru_title }}</h2>
             <div class="catalog__filter">
                 <div class="catalog__sort">
+
                     <div class="catalog__sortItem">
                         <div class="catalog__sortCurrent" id="catalogType">
                             <span class="lbl">All types&nbsp; <i class="fa fa-angle-down"></i></span>
+                           <div class="" style="width: auto; padding: 0; white-space: nowrap; margin-top: 0; box-shadow: 0 0 62px rgba(20, 47, 106, 0.47); border-radius: 8px 10px 10px; background-color: #ffffff;" uk-dropdown="mode: hover; offset: 30">
+                              <ul class="uk-nav catalog__sort__dropdown">
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Мои талоны</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Корзина услуг</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Заявление на прикрепление</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Запись на прием</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Диспансеризация</a></li>
+                              </ul>
+                           </div>
                         </div>
-
                     </div>
+
                     <div class="catalog__sortItem">
                         <div class="catalog__sortCurrent" id="catalogPeople">
                             <span class="lbl">Категории&nbsp; <i class="fa fa-angle-down"></i></span>
+                           <div class="" style="width: auto; padding: 0; white-space: nowrap; margin-top: 0; box-shadow: 0 0 62px rgba(20, 47, 106, 0.47); border-radius: 8px 10px 10px; background-color: #ffffff;" uk-dropdown="mode: hover; offset: 30">
+                              <ul class="uk-nav catalog__sort__dropdown">
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Мои талоны</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Корзина услуг</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Заявление на прикрепление</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Запись на прием</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Диспансеризация</a></li>
+                              </ul>
+                           </div>
                         </div>
-
                     </div>
+
                     <div class="catalog__sortItem">
                         <div class="catalog__sortCurrent" id="catalogBrand">
                             <span class="lbl">Бренд&nbsp; <i class="fa fa-angle-down"></i></span>
+                           <div class="" style="width: auto; padding: 0; white-space: nowrap; margin-top: 0; box-shadow: 0 0 62px rgba(20, 47, 106, 0.47); border-radius: 8px 10px 10px; background-color: #ffffff;" uk-dropdown="mode: hover; offset: 30">
+                              <ul class="uk-nav catalog__sort__dropdown">
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Мои талоны</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Корзина услуг</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Заявление на прикрепление</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Запись на прием</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Диспансеризация</a></li>
+                              </ul>
+                           </div>
                         </div>
                     </div>
+
                     <div class="catalog__sortItem">
                         <div class="catalog__sortCurrent" id="popularity">
                             <span class="lbl">Сортировать&nbsp; <i class="fa fa-angle-down"></i></span>
+                           <div class="" style="width: auto; padding: 0; white-space: nowrap; margin-top: 0; box-shadow: 0 0 62px rgba(20, 47, 106, 0.47); border-radius: 8px 10px 10px; background-color: #ffffff;" uk-dropdown="mode: hover; offset: 30">
+                              <ul class="uk-nav catalog__sort__dropdown">
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Мои талоны</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Корзина услуг</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Заявление на прикрепление</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Запись на прием</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Диспансеризация</a></li>
+                              </ul>
+                           </div>
                         </div>
                     </div>
+
                     <div class="catalog__sortItem">
                         <div class="catalog__sortCurrent" id="catalogPrice">
                             <span class="lbl">Price&nbsp; <i class="fa fa-angle-down"></i></span>
+                           <div class="" style="width: auto; padding: 0; white-space: nowrap; margin-top: 0; box-shadow: 0 0 62px rgba(20, 47, 106, 0.47); border-radius: 8px 10px 10px; background-color: #ffffff;" uk-dropdown="mode: hover; offset: 30">
+                              <ul class="uk-nav catalog__sort__dropdown">
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Мои талоны</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Корзина услуг</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Заявление на прикрепление</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Запись на прием</a></li>
+                                 <li class="catalog__sort__dropdown__item"><a href="#">Диспансеризация</a></li>
+                              </ul>
+                           </div>
                         </div>
                     </div>
+
                 </div>
             </div>
+            
             <div uk-slideshow="animation: push; max-height: 262;" class="catalog-slider" index="1">
                 <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
                     <ul class="uk-slideshow-items">
