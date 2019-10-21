b140d377ead44d7b9c11f6e6053b0559eab7542b
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-21 20:02:17 +0500

    Change redirect link, do not validate name, attach simple role

--- app/Http/Controllers/FrontAuth/RegisterController.php
+++ app/Http/Controllers/FrontAuth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\FrontAuth;
 
 use App\User;
+use App\Role;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -28,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/login';
+    protected $redirectTo = '/request';
 
     /**
      * Create a new controller instance.
@@ -49,9 +50,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
         ]);
     }
 
@@ -63,10 +63,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+        $user = User::create([
+            'name' => $data['email'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        $user
+            ->roles()
+            ->attach(Role::where('name', 'simple_user')->first());
+            
+        return $user;
     }
 }
