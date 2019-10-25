8edadd83be69385526ee0ee96e7d1e60ff88e411
Дмитрий <dmitriyl1899@gmail.com>
2019-10-25 17:43:14 +0500

    Fix documentation

--- app/User.php
+++ app/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
      *
      * @param string|array $roles
      *
-     * @return void
+     * @return boolean
      */
     public function authorizeRoles($roles)
     {
