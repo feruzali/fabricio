2732318c437992f48c022c801ca0730e62c265d6
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-29 00:18:19 +0500

    Group categories by parent

--- app/Http/Middleware/CatalogMiddleware.php
+++ app/Http/Middleware/CatalogMiddleware.php
@@ -19,7 +19,7 @@ class CatalogMiddleware
     public function handle($request, Closure $next)
     {
         View::share('contact', Contact::find(1));
-        View::share('categories', Categories::all());
+        View::share('categories', Categories::where('parent_id', '=', null));
         if (session()->has('cart')) {
             $cartTotalSum = 0;
             $cartTotalCount = 0;
