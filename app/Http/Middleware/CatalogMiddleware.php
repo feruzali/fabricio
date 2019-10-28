e57d35f6693a00eb54997817666e490eebf8a645
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-29 00:25:25 +0500

    Fix getting categories

--- app/Http/Middleware/CatalogMiddleware.php
+++ app/Http/Middleware/CatalogMiddleware.php
@@ -19,7 +19,7 @@ class CatalogMiddleware
     public function handle($request, Closure $next)
     {
         View::share('contact', Contact::find(1));
-        View::share('categories', Categories::where('parent_id', '=', 0));
+        View::share('categories', Categories::where('parent_id', '=', null)->get());
         if (session()->has('cart')) {
             $cartTotalSum = 0;
             $cartTotalCount = 0;
