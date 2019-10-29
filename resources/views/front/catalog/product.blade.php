637e07b395a5cc15687bdd7326d812224f643c8c
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-30 00:28:51 +0500

    Product slider from database

--- resources/views/front/catalog/product.blade.php
+++ resources/views/front/catalog/product.blade.php
@@ -5,57 +5,57 @@
 @section('content')
     <section class="card" id="card">
         <div class="container">
-{{--            @foreach($product->colors as $color)--}}
-{{--                <div uk-slideshow class="card--slider" data-color="{{ $color->id }}">--}}
+            @foreach($product->colors as $key => $productColor)
+                <div uk-slideshow class="card--slider" data-color="{{ $key }}">
+                    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
+                        <ul class="uk-slideshow-items card-slideshow__bg">
+                            @foreach($productColor->images as $productImage)
+                            <li class="card-slideshowItem">
+                                <img class="uk-position-center uk-margin-large-left" src="{{ $productImage->getImage() }}" alt="">
 
-{{--                    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">--}}
+                                <div class="uk-overlay uk-position-bottom-right uk-position-large">
+                                    <p class="card-slideshow">{{ $product->title }}</p>
+                                </div>
+                            </li>
+                            @endforeach
+                        </ul>
 
-{{--                        <ul class="uk-slideshow-items card-slideshow__bg" style="min-height: 624.375px;">--}}
-{{--                            @foreach ($color->images as $image)--}}
-{{--                                <li class="card-slideshowItem">--}}
-{{--                                    <img class="uk-position-center uk-margin-large-left" src="{{ $image->getImage() }}" alt="">--}}
-
-{{--                                    <div class="uk-overlay uk-position-bottom-right uk-position-large">--}}
-{{--                                        <p class="card-slideshow">Мужские солнцезащитные очки + ФУТЛЯР</p>--}}
-{{--                                    </div>--}}
-{{--                                </li>--}}
-{{--                            @endforeach--}}
-
-{{--                        </ul>--}}
-
-{{--                        <div class="uk-position-bottom-right uk-position-medium card-slideshow__nav">--}}
-{{--                            <i class="fa fa-long-arrow-left" uk-slideshow-item="previous"></i>--}}
-{{--                            <i class="fa fa-long-arrow-right" uk-slideshow-item="next"></i>--}}
-{{--                        </div>--}}
-{{--                        <div class="uk-position-center-right uk-position-large">--}}
-{{--                            <ul class="uk-slideshow-nav uk-dotnav uk-dotnav-vertical"></ul>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="uk-position-center-left uk-position-small">--}}
-{{--                            @foreach($product->colors as $key => $prodColor)--}}
-{{--                                <div class="circle-wrapper @if($key === 1) circle-wrapper--active @endif" data-color="{{ $prodColor->id }}">--}}
-{{--                                    <div class="circle" style="background-color: {{ $prodColor->colorHEX }};"><i class="fa fa-check"></i></div>--}}
-{{--                                </div>--}}
-{{--                            @endforeach--}}
-{{--                        </div>--}}
-
-{{--                        <div class="uk-position-bottom-left uk-position-small">--}}
-{{--                            <div class="stepper stepper--style-3 js-spinner card-stepper">--}}
-{{--                                <input autofocus type="number" min="1" max="10" step="1" value="1" class="stepper__input card-stepper__input quantity-field">--}}
-{{--                                <div class="stepper__controls">--}}
-{{--                                    <button style="background-color: transparent; right: 18px;" type="button" spinner-button="up"><i class="fa fa-chevron-right"></i></button>--}}
-{{--                                    <button style="background-color: transparent;" type="button" spinner-button="down"><i class="fa fa-chevron-left"></i></button>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-
-{{--                            <button class="card-slideshow__btn add-to-card-button" data-product-id="{{ $product->id }}"><i class="fa fa-shopping-cart"></i>&nbsp; Добавить в корзину</button>--}}
-{{--                        </div>--}}
-
-{{--                    </div>--}}
-
-{{--                </div>--}}
-{{--            @endforeach--}}
-            <div uk-slideshow class="card--slider" data-color="black">
+                        <div class="uk-position-bottom-right uk-position-medium card-slideshow__nav">
+                            <i class="fa fa-long-arrow-left" uk-slideshow-item="previous"></i>
+                            <i class="fa fa-long-arrow-right" uk-slideshow-item="next"></i>
+                        </div>
+                        <div class="uk-position-center-right uk-position-large card-slideshow__dotnav">
+                            <ul class="uk-slideshow-nav uk-dotnav uk-dotnav-vertical"></ul>
+                        </div>
+                        <div class="uk-position-center-left uk-position-small card-colors">
+                            @foreach ($product->colors as $colorKey => $color)
+                                <div class="circle-wrapper circle-wrapper--active" data-color="{{ $colorKey }}">
+                                    <div class="circle" style="background-color: #4986ff;"><i class="fa fa-check"></i></div><span>@if($colorKey == $key) {{ $color->name }} @endif</span>
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="uk-position-bottom-left uk-position-small md_hidden">
+                            <div class="stepper stepper--style-3 js-spinner card-stepper">
+                                <input autofocus type="number" min="1" max="10" step="1" value="1" class="stepper__input card-stepper__input quantity-field">
+                                <div class="stepper__controls">
+                                    <button style="background-color: transparent; right: 18px;" type="button" spinner-button="up"><i class="fa fa-chevron-right"></i></button>
+                                    <button style="background-color: transparent;" type="button" spinner-button="down"><i class="fa fa-chevron-left"></i></button>
+                                </div>
+                            </div>
+                            <button class="card-slideshow__btn add-to-card-button" data-product-id="{{ $product->id }}"><i class="fa fa-shopping-cart"></i>&nbsp; Добавить в корзину</button>
+                        </div>
+                        <div class="stepper stepper--style-3 js-spinner card-stepper md_visible">
+                            <input autofocus type="number" min="1" max="10" step="1" value="1" class="stepper__input card-stepper__input">
+                            <div class="stepper__controls">
+                                <button style="background-color: transparent; right: 18px;" type="button" spinner-button="up"><i class="fa fa-chevron-right"></i></button>
+                                <button style="background-color: transparent;" type="button" spinner-button="down"><i class="fa fa-chevron-left"></i></button>
+                            </div>
+                        </div>
+                        <button class="card-slideshow__btn md_visible"><i class="fa fa-shopping-cart"></i>&nbsp; Добавить в корзину</button>
+                    </div>
+                </div>
+            @endforeach
+            <!--<div uk-slideshow class="card--slider" data-color="black">
 
                 <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
 
@@ -287,7 +287,7 @@
 
                 </div>
 
-            </div>
+            </div> -->
 
             <h2 class="card__title">{{ $product->title }}</h2>
         </div>
