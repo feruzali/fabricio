662ca03f02d246ffcf10381dd7a05e5f9881ff37
Feruz <feruzprivet@gmail.com>
2019-11-18 23:44:17 +0500

    Create 'confirm' page

---
+++ resources/views/front/confirm_page.php
@@ -0,0 +1,27 @@
+<?php include 'header.php'; ?>
+
+
+<section id="confirm" class="confirm">
+	
+	<div class="container">
+		<h2 class="page-title">Заказ подтверждён</h2>
+		
+		<div class="confirm-order">
+			<div class="confirm-order__title">Спасибо за покупку!</div>
+			<div class="confirm-order__num">
+				Номер заказа: 001998976
+			</div>
+			<div class="confirm-order__descr">
+			Вы получите письмо на ваш адрес электронной почты (email) с подробной информацией о заказе.
+			</div>
+			<a href="catalog_page.php" class="confirm-order__btn">Вернуться к выбору товаров</a>
+		</div>
+
+	</div>
+
+</section>
+
+
+<?php include 'feedback.php'; ?>
+
+<?php include 'footer.php'; ?>
